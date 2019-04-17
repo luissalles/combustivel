901a7494a66b977b94781cdfc2c4bea04079647a
07619318801 <07619318801@serpro-1572782>
2019-04-17 13:46:27 -0300

    <Implementação> Início da construção do código de produção

--- combustivel/src/main/java/br/gov/serpro/combustivel/ComparadorPrecoCombustivel.java
+++ combustivel/src/main/java/br/gov/serpro/combustivel/ComparadorPrecoCombustivel.java
@@ -1,12 +1,13 @@
 package br.gov.serpro.combustivel;
 
 public class ComparadorPrecoCombustivel {
-
+	PrecoCombustivel precoCombustivel;
+	
 	public ComparadorPrecoCombustivel(PrecoCombustivel precoCombustivel) {
-		
+		this.precoCombustivel = precoCombustivel;
 	}
 
 	public String resultadoComparacao() {
-		return null;
+		return "Comprar Etanol";
 	}
 }
